ad19982e14edf05af10d3229c0275ee7831d006a
System Administrator <root@Jamess-MacBook-Pro.local>
2023-05-22 00:29:09 -0400

    feature update

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -16,10 +16,19 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 	const pathName = usePathname();
 	const router = useRouter();
 
+	const handleProfileClick = () => {
+		if (post.creator._id === session?.user.id) return router.push('/profile');
+
+		router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+	};
+
 	return (
 		<div className='prompt_card'>
 			<div className='flex justify-between items-start gap-5 cursor-pointer'>
-				<div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
+				<div
+					className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+					onClick={handleProfileClick}
+				>
 					<Image
 						src={post.creator.image}
 						alt='user_image'
@@ -36,6 +45,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 						</p>
 					</div>
 				</div>
+
 				<div className='copy_btn' onClick={handleCopy}>
 					<Image
 						src={
